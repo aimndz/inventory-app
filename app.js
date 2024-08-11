c69b91c193e05948b63682d7b69c9b720388cdf7
Amiel Ian Mendoza <amielmendoza143@gmail.com>
2024-08-11 18:54:01 +0800

    Chore: Change serve static files to 'public' directory

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(baseRouteMiddleware);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "")));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/", indexRouter);
 app.use("/genres", genresRouter);
